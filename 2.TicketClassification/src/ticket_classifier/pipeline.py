a675290520b8b1142cae219aef10b9ba03d65ad3
Mahsa Barghi Mehmandari <mahsabarghimehmandari@gmail.com>
2026-01-26 15:12:13 +0100

    feat: update pipline.py and train.py to support new classifier:SVC

--- 2.TicketClassification/src/ticket_classifier/pipeline.py
+++ 2.TicketClassification/src/ticket_classifier/pipeline.py
@@ -1,28 +1,36 @@
 # pipeline.py
-"""
-Skeleton for the Ticket Classification Pipeline
-Preprocessing and model will live here
-"""
+
 # from sklearn.dummy import DummyClassifier # Just for a dummy run
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
 from sklearn.pipeline import Pipeline
 # from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 # from sklearn.base import BaseEstimator, TransformerMixin
 import joblib
 class TicketClassifierPipeline:
-    def __init__(self):
-        # Placeholder for future pipeline
+    def __init__(self, classifier='logreg'):
+        """
+        classifier:'logreg' for LogisticRegression, 'svc' for LinearSVC
+        """
+        self.classifier_name = classifier
         self.pipeline = None
 
     def build_pipeline(self):
         """
-        Build the sklearn pipeline with vectorizer, transformer, and classifier
+        Build the sklearn pipeline with vectorizer, and classifier
         """
-        # Example placeholder: we will fill this later
+        if self.classifier_name =='logreg':
+             clf = LogisticRegression(max_iter=1000, class_weight='balanced')
+        elif self.classifier_name == 'svc':
+            clf = LinearSVC(max_iter=5000, C=1.5, class_weight='balanced', loss='squared_hinge')
+        else:
+            raise ValueError(f"Classifier {self.classifier_name} is not supported.")
+
+
         self.pipeline = Pipeline([
-            ('tfidf', TfidfVectorizer(max_features=5000, ngram_range=(1,2), stop_words='english')),
-            ('clf', LogisticRegression(max_iter=1000, C=2, class_weight='balanced')) 
+            ('tfidf', TfidfVectorizer(max_features=10000, ngram_range=(1,3), min_df=2, max_df=0.95, stop_words='english')),
+            ('clf', clf) 
         ])
         return self.pipeline
 
--- 2.TicketClassification/src/ticket_classifier/train.py
+++ 2.TicketClassification/src/ticket_classifier/train.py
@@ -1,9 +1,10 @@
 # train.py
-"""
+"""import argparse
 Training script for Ticket Classification Pipeline
 """
 
 import random
+import argparse
 from .data_loader import load_ticket_dataset
 from .pipeline import TicketClassifierPipeline
 from .evaluate import evaluate_model
@@ -18,6 +19,21 @@ random.seed(RANDOM_SEED)
 # Main training function
 # ----------------------
 def main():
+        # ----------------------
+    # Parse arguments
+    # ----------------------
+    parser = argparse.ArgumentParser(description="Train Ticket Classification Pipeline")
+    parser.add_argument(
+        "--classifier",
+        type=str,
+        default="logreg",
+        choices=["logreg", "svc"],
+        help="Classifier to train: logreg (LogisticRegression) or svc (LinearSVC)"
+    )
+    args = parser.parse_args()
+
+    print(f"Selected classifier: {args.classifier}")
+
     # ----------------------
     # Load dataset
     # ----------------------
@@ -28,14 +44,14 @@ def main():
     # ----------------------
     # Initialize the pipeline
     # ----------------------
-    clf_pipeline = TicketClassifierPipeline()
+    clf_pipeline = TicketClassifierPipeline(classifier=args.classifier)
     clf_pipeline.build_pipeline()
 
     # ----------------------
     # Fit the pipeline
     # ----------------------
     clf_pipeline.fit(X_train, y_train)
-    print("Pipeline trained on ticket dataset (Top 5 classes).")
+    print(f"Pipeline trained on ticket dataset (Top 5 classes) using {args.classifier}.")
 
     # ----------------------
     # Evaluate
